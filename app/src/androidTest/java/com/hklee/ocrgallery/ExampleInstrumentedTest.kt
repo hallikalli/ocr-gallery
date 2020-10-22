d7f43b1d278af0208c05a9361c1ff0cc4300c7c3
HyunKyungLee <hklee.sw@gmail.com>
2020-10-22 19:24:09 +0900

    tesseract, mediastore test

--- app/src/androidTest/java/com/hklee/ocrgallery/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/hklee/ocrgallery/ExampleInstrumentedTest.kt
@@ -1,12 +1,21 @@
 package com.hklee.ocrgallery
 
+import android.content.ContentResolver
+import android.database.Cursor
+import android.net.Uri
+import android.provider.MediaStore
+import android.provider.MediaStore.Images.Media.query
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.hklee.ocrgallery.utilites.TesseractOcr
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import timber.log.Timber
+import java.sql.Date
+import java.util.concurrent.TimeUnit
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,4 +30,51 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.hklee.OcrGallery", appContext.packageName)
     }
+
+    @Test
+    fun tesseract() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        TesseractOcr(appContext)
+    }
+
+    @Test
+    fun accessMediaStore() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val projection = arrayOf(
+            MediaStore.Images.Media._ID,
+            MediaStore.Images.Media.DISPLAY_NAME,
+            MediaStore.Images.Media.DATE_TAKEN,
+        )
+        val selection = "UPPER(${MediaStore.Images.Media.DISPLAY_NAME}) LIKE UPPER('%Screenshot%')"
+        val sortOrder = "${MediaStore.Images.Media.DATE_TAKEN} DESC"
+        val cursor = appContext.contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            selection,
+            null,
+            sortOrder
+        )
+        cursor?.use {
+            val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+            val dateTakenColumn =
+                cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATE_TAKEN)
+            val displayNameColumn =
+                cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME)
+            while (cursor.moveToNext()) {
+                val id = cursor.getLong(idColumn)
+                val dateTaken = Date(cursor.getLong(dateTakenColumn))
+                val displayName = cursor.getString(displayNameColumn)
+                val contentUri = Uri.withAppendedPath(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    id.toString()
+                )
+                Timber.tag("accessMediaStore()").d(
+                    "id: $id, display_name: $displayName, date_taken: " +
+                            "$dateTaken, content_uri: $contentUri"
+                )
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/hklee/ocrgallery/utilites/TesseractOcr.kt
+++ app/src/main/java/com/hklee/ocrgallery/utilites/TesseractOcr.kt
@@ -3,24 +3,26 @@ package com.hklee.ocrgallery.utilites
 import android.content.Context
 import android.graphics.Bitmap
 import com.googlecode.tesseract.android.TessBaseAPI
-import com.hklee.ocrgallery.R
 import timber.log.Timber
 import java.io.*
 
 
 class TesseractOcr(val context: Context) {
 
-    private val ASSETS_TESS = "tessdata"
+    private val TRANINEDDATA_FOLDER = "tessdata"
     private val LANG = "kor+eng"
-    private var trainedDataPath = context.cacheDir.absolutePath + "/tessdata/"
+    private var TESSERACT_PATH = context.cacheDir.absolutePath + "/tesseract/"
+
+    //파일 구조
+    // /data/user/0/~/cache/tesseract/tessdata/eng.traineddata
 
     private val tess = TessBaseAPI().apply {
         updateTrainedData()
-        init(trainedDataPath, LANG)
+        init(TESSERACT_PATH, LANG) //init시 tessdata 라는 하위폴더가 가 있어야함
     }
 
     // 문자 인식 및 결과 출력
-    fun toOcrText(bitmap: Bitmap?): String? {
+    fun toOcrText(bitmap: Bitmap): String {
         tess.setImage(bitmap)
         return tess.utF8Text
     }
@@ -39,33 +41,33 @@ class TesseractOcr(val context: Context) {
     }
 
     private fun prepareDirectory(): Boolean {
-        val dir = File(trainedDataPath)
+        val dir = File( TESSERACT_PATH+TRANINEDDATA_FOLDER)
         if(dir.exists()) return true
         return dir.mkdir()
     }
 
     private fun updateTrainedDataFile() {
-        var trainedDataList = context.assets.list(ASSETS_TESS)
-        if (trainedDataList == null) {
+        var assetsList = context.assets.list(TRANINEDDATA_FOLDER)
+        if (assetsList == null) {
             Timber.e("No data in assets/tessdata")
             return
         }
-        for (fileName in trainedDataList) {
-            val path = trainedDataPath+fileName
-            if (!File(path).exists()) continue
-            copyFile("$ASSETS_TESS/$fileName", path)
+        for (fileName in assetsList) {
+            val tessFileName = TESSERACT_PATH+TRANINEDDATA_FOLDER+"/"+fileName
+            if (!File(tessFileName).exists())
+                copyFile("$TRANINEDDATA_FOLDER/$fileName", tessFileName)
         }
     }
 
     private fun copyFile(src: String, dest: String) {
         try {
             context.assets.open(src).use { input ->
-                FileOutputStream(dest).use { output ->
+                FileOutputStream(dest,false).use { output ->
                     input.copyTo(output)
                 }
             }
         } catch (e: IOException) {
-            e.printStackTrace()
+            Timber.e(e.printStackTrace().toString())
         }
     }
 }
