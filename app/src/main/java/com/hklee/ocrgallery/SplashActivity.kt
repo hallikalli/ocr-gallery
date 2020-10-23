1cfc8311b617e80fcf1c56976a3e933f0a36d7e6
HyunKyungLee <hklee.sw@gmail.com>
2020-10-23 20:15:01 +0900

    스플래시, 런처아이콘 추가

---
+++ app/src/main/java/com/hklee/ocrgallery/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.hklee.ocrgallery
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    private val SPLASH_DISPLAY_TIME: Long = 2000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_DISPLAY_TIME);
+    }
+}
